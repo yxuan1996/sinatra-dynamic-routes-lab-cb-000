92b3c75a03ed188c48bc841a94a756c47b558d5f
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-03 19:01:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-
+   final_string = ""
+   (params[:number].to_i).times do
+     final_string += "#{params[:phrase]}\n"
+   end
+   final_string
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
