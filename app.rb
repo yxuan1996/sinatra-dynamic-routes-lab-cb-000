b21770471946d9e879f3f15709138a3905505d56
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-03 18:49:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+  get '/reversename/:name' do
+    
+  end
+
+  get '/square/:number' do
+    
+  end
+
+  get '/say/:number/:phrase' do
+    
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    
+  end
+
+  get '/:operation/:number1/:number2' do
+    
+  end
 
 end
