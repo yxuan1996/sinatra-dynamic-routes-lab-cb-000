38b24e6af7a7e54054dc3e94f3b416cd56bc9021
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-03 18:53:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,9 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-
+    @number = params[:number].to_i
+    @answer = @number*@number
+    "#{@answer}"
   end
 
   get '/say/:number/:phrase' do
