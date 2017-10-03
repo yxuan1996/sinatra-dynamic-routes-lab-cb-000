0058002de58b231413e71efc01e9e3d2e7a6b782
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-03 18:52:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/reversename/:name' do
     @name = params[:name].reverse
-    '#{@name}'
+    "#{@name}"
   end
 
   get '/square/:number' do
